fdb6d7aa44dbb5ef4d53dbc7ecbc9fec0d24f397
MATHEUS WAGATUMA SPADRIZANI <spadrizani.8935@aluno.pr.senac.br>
2023-03-24 08:21:34 -0300

    cadastro atualizado

--- p1.php
+++ p1.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Concert+One&display=swap" rel="stylesheet">
@@ -30,8 +31,10 @@
             <input type="text" name="email"/><br/><br/>
             <label>Senha: </label>
             <input type="text" name="senha"/><br/><br/>
-            <input type="submit" value="Entrar">
-            <input type="reset" value="Limpar">
+            <button id="enviar" type="submit" class="btn btn-warning">Enviar</button>
+            <div class="Mario">
+            <button id="limpar" type="reset" class="btn btn-warning">Limpar</button>
+            </div>
         </form>
     </body>
 </html>
